a0072da9290574874a1a0d2db8cdf4d81cf1880e
vibha <vibha@vinsol.com>
2008-06-25 11:57:51 +0600

    Modified Application and SignupPlans controller

--- app/controllers/admin/signup_plans_controller.rb
+++ app/controllers/admin/signup_plans_controller.rb
@@ -2,5 +2,8 @@ class Admin::SignupPlansController < ApplicationController
   layout 'admin'
   before_filter :current_user 
   before_filter :authorized_as_admin
-  active_scaffold :signup_plan
+  active_scaffold :signup_plan do |config|
+    config.label = "SignupPlans"
+    config.columns = [:name, :price, :courses, :time_period, :tutors, :students]
+  end  
 end  
--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  
+  ActiveScaffold.set_defaults do |config|
+    config.ignore_columns.add [:created_at, :updated_at, :status]
+  end
  
   helper :all # include all helpers, all the time
   # See ActionController::RequestForgeryProtection for details
