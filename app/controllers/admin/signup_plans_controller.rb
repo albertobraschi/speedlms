3c4dd43ffbe1fc54aeb1a958d7529f31b3f35273
vibha <vibha@vinsol.com>
2008-06-30 17:44:41 +0600

    Added a label to tutors for unlimited plans and a method to model

--- app/controllers/admin/signup_plans_controller.rb
+++ app/controllers/admin/signup_plans_controller.rb
@@ -5,5 +5,11 @@ class Admin::SignupPlansController < ApplicationController
   active_scaffold :signup_plan do |config|
     config.label = "SignupPlans"
     config.columns = [:name, :price, :no_of_courses, :time_period, :no_of_tutors, :no_of_students]
-  end  
+    #debugger
+    columns[:no_of_courses].label = "Courses"
+    columns[:no_of_tutors].label = "Tutors"
+    columns[:no_of_students].label = "Students"
+    columns[:no_of_tutors].description = "Please enter a number > 10000 to make this field infinite"
+  end 
+  
 end  
--- app/models/signup_plan.rb
+++ app/models/signup_plan.rb
@@ -1,8 +1,18 @@
 class SignupPlan < ActiveRecord::Base
+  INFINITE_DEFINITION = 10000
   has_many :users
+  @signup_plan = SignupPlan.new
   
   def self.find_plans
 	  find(:all, :order => 'price ASC')
 	end
+	
+	def is_unlimited?
+	  if @signup_plan.no_of_tutors > INFINITE_DEFINITION
+	    return true
+	  else
+	    return false  
+	  end  
+	end  
 	
 end
