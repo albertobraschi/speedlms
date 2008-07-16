9f1e1b33020ac52823a3c5acf294a2be065bbef8
jitendra <jitendra@MARS.(none)>
2008-07-16 10:08:31 +0530

    added active wheel for ajaxified check availability of username and subdomain,removed login and signup links for logged in user

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file
-
+FREE = 0.0
 # Uncomment below to force Rails into production mode when
 # you don't control web/app server and can't set it the proper way
 ENV['RAILS_ENV'] ||= 'development'
